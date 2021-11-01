52307be92be7a54712f3399a65414b56ba2c85e8
FDGarciaC <franciscodanielgarcia@ibermutua.es>
2021-11-01 21:47:19 +0100

    Added recycler view separator

--- app/src/main/java/com/fdanielgarcia/mygermanvocabulary/presentation/SearchFragment.kt
+++ app/src/main/java/com/fdanielgarcia/mygermanvocabulary/presentation/SearchFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.fdanielgarcia.mygermanvocabulary.R
 import com.fdanielgarcia.mygermanvocabulary.data.VocabularyList
@@ -42,6 +43,7 @@ class SearchFragment : Fragment() {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context)
             adapter = searchResultAdapter
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
         }
 
         binding.editTextSearch.setText("")
