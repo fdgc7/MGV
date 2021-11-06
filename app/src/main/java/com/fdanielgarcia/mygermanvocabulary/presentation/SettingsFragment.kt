3d775a5b318e50a9e5258779abc62a355bafc536
FDGarciaC <franciscodanielgarcia@ibermutua.es>
2021-11-06 23:02:09 +0100

    Implemented Searching Min Chars

--- app/src/main/java/com/fdanielgarcia/mygermanvocabulary/presentation/SettingsFragment.kt
+++ app/src/main/java/com/fdanielgarcia/mygermanvocabulary/presentation/SettingsFragment.kt
@@ -1,13 +1,43 @@
 package com.fdanielgarcia.mygermanvocabulary.presentation
 
 import android.os.Bundle
-import androidx.preference.PreferenceFragmentCompat
+import android.widget.Toast
+import androidx.preference.*
 import com.fdanielgarcia.mygermanvocabulary.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    companion object {
+        const val INF_NUM_CHAR_LIMIT = 0
+        const val SUP_NUM_CHAR_LIMIT = 10
+    }
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.settings, rootKey)
 
+        val searchingMinChars =
+            findPreference<EditTextPreference>("edit_text_preference_searching_min_chars")
 
+        searchingMinChars?.setOnPreferenceChangeListener { preference, newValue ->
+            var value = -1
+            try {
+                value = Integer.parseInt(newValue as String)
+            } catch (e: NumberFormatException) {
+                Toast.makeText(
+                    activity,
+                    activity?.resources?.getString(R.string.incorrect_value),
+                    Toast.LENGTH_LONG
+                ).show()
+                false
+            }
+            if (value < INF_NUM_CHAR_LIMIT || value > SUP_NUM_CHAR_LIMIT) {
+                Toast.makeText(
+                    activity,
+                    activity?.resources?.getString(R.string.incorrect_value),
+                    Toast.LENGTH_LONG
+                ).show()
+                false
+            } else { true }
+        }
     }
 }
+
